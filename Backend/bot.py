8181c0c58e44f0695d4fd1b2e521d07cd82bcb51
Damir Afliatonov <88425319+Probirochniy@users.noreply.github.com>
2022-06-25 17:19:41 +0300

    Update bot.py

--- Backend/bot.py
+++ Backend/bot.py
@@ -1,14 +1,12 @@
 import logging
 from telethon import TelegramClient, sync, events
-import aiogram.utils.markdown as md
 from aiogram import Bot, Dispatcher, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.filters import Text
 from aiogram.dispatcher.filters.state import State, StatesGroup
-from aiogram.types import ReplyKeyboardRemove, ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, \
+from aiogram.types import InlineKeyboardMarkup, \
     InlineKeyboardButton
-from aiogram.types import ParseMode
 from aiogram.utils import executor
 import databaseHandler
 
